893ebfed9241f55451656fb99b00960a4262c088
Sailor <la237887@student.helha.be>
2025-12-27 16:34:33 +0100

    redirection auto si user connecté

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, OnInit, signal} from '@angular/core';
+import {Router, RouterOutlet} from '@angular/router';
+import {AuthService} from './core/services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,24 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('ClinikTimeFrontend');
+
+  constructor(
+    private authService : AuthService,
+    private router : Router
+  ) {}
+
+  ngOnInit() {
+    this.authService.checkAuth().subscribe({
+      next: () => {
+        //utilisateur déjà connecté
+        this.router.navigate(['/home']);
+      },
+      error : () => {
+        //utilisateur non connecté
+        this.router.navigate(['/login']);
+      }
+    });
+  }
 }
